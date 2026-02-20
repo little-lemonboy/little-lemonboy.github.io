93826b93127b73ef62581e0590b44119dda9e426
little-lemonboy <87843013+little-lemonboy@users.noreply.github.com>
2026-02-20 02:22:34 -0600

    Update script.js

--- script.js
+++ script.js
@@ -177,8 +177,9 @@ document.addEventListener('mouseup', function() {
     isDragging = false; currentWindow = null; 
     
     if (isDraggingIcon && currentIcon) {
-        var currentX = parseInt(currentIcon.style.left) || 0;
-        var currentY = parseInt(currentIcon.style.top) || 0;
+        var currentX = currentIcon.offsetLeft;
+        var currentY = currentIcon.offsetTop;
+        
         var snappedX = Math.max(10, Math.round((currentX - 10) / 90) * 90 + 10);
         var snappedY = Math.max(10, Math.round((currentY - 10) / 90) * 90 + 10);
         currentIcon.style.left = snappedX + "px";
